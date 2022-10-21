919694dcd40acbdf0f5e95c96a162fe33d014dc7
MatheusParada <111541466+MatheusParada@users.noreply.github.com>
2022-10-20 22:25:33 -0300

    Add files via upload

--- Pessoa/src/br/com/exemploclassearraylist/Principal.java
+++ Pessoa/src/br/com/exemploclassearraylist/Principal.java
@@ -6,22 +6,47 @@ import javax.swing.JOptionPane;
     public class Principal {
 
         public static void main(String[] args) {
-            ArrayList<Pessoa> pessoas = new ArrayList <> ();
+            ArrayList<Pessoa> pessoas = new ArrayList<>();
+            Pessoa p1;
+            int opcao;
 
-            String nome= JOptionPane.showInputDialog("Digite o Nome");
-            int idade= Integer.parseInt(JOptionPane.showInputDialog("Digite a Idade"));
-            String cpf= JOptionPane.showInputDialog("Digite o CPF");
-            Pessoa p1 = new Pessoa(nome, idade, cpf);
-            pessoas.add(p1);
+            do {
+                opcao = Integer.parseInt(JOptionPane.showInputDialog("<1> cadastrar\n<2>Visualizar\n<3>Sair>"));
+                switch (opcao) {
 
+                    case 1:
+                        String nome = JOptionPane.showInputDialog("Digite o Nome");
+                        int idade = Integer.parseInt(JOptionPane.showInputDialog("Digite a Idade"));
+                        String cpf = JOptionPane.showInputDialog("Digite o CPF");
+                        p1 = new Pessoa(nome, idade, cpf);
+                        pessoas.add(p1);
+                        break;
 
-            int i = 0;
-            for (Pessoa contato: pessoas) {
-                System.out.printf("Posiçao %d- %s %s %d\n", i, contato.getNome(), contato.getCpf(), contato.getIdade() );
-                i++;
-            }
+                    case 2:
+                        int i = 0;
+                        for (Pessoa contato : pessoas) {
+                            JOptionPane.showMessageDialog(null, i + " " + contato.getNome() + " " + contato.getCpf() + " " + contato.getIdade(), "Pessoas", JOptionPane.DEFAULT_OPTION);
+                            i++;
+                        }
+                        break;
+                    case 3:
+                        String buscar = JOptionPane.showInputDialog(("nome para excluir"));
+                        int j;
 
-        }
+                        for (j = 0; j < pessoas.size(); j++) {
+                            if (pessoas.get(j).getNome().equals(buscar))
+                            {
 
+                                pessoas.remove(j);
+                            JOptionPane.showMessageDialog(null,"removido com sucesso", "pessoas", JOptionPane.DEFAULT_OPTION);
+                        }
+                }
+                        break;
+                case 4:
+                    break;
+
+                }
+        }while(opcao !=3);
+    }
     }
 
